add95ca0bf37f0417dea6fe16ddc103839c0a83f
BDKPlayer <f-absen@hotmail.de>
2019-10-01 13:20:21 +0200

    added colorpicker to each player

--- AoE_IMGUI/UnitCollisions.cpp
+++ AoE_IMGUI/UnitCollisions.cpp
@@ -6,7 +6,7 @@
 #include "Input.h"
 
 
-void DrawBox(Unit* unit)
+void DrawBox(Unit* unit, int32_t color)
 {
 	Vector3 one3 = unit->vPos;
 	one3.x -= unit->pUnitData->collisionX;
@@ -33,21 +33,32 @@ void DrawBox(Unit* unit)
 	ImVec2 ivThree = ImVec2(three.x, three.y);
 	ImVec2 ivFour = ImVec2(four.x, four.y);
 
-	Renderer::Get()->RenderRect(ivOne, ivFour, ivTwo, ivThree, 0xff0000ff);
+	Renderer::Get()->RenderRect(ivOne, ivFour, ivTwo, ivThree, color);
 }
 
 void UnitCollisions::OnUnitIteration(Unit* unit, Player* player, int playerIndex)
 {
 	if (playerEsp[playerIndex])
 	{
-		DrawBox(unit);
+		DrawBox(unit, colors_hex[playerIndex]);
 	}
 }
 
 void UnitCollisions::OnMenuPlayerTreenode(Player * player, int playerIndex)
 {
-	ImGui::Checkbox("ESP", &playerEsp[playerIndex]);
-
+	if (ImGui::TreeNode("ESP"))
+	{
+		ImGui::PushItemWidth(100);
+		ImGui::Checkbox("Enabled", &playerEsp[playerIndex]);
+		if (ImGui::ColorPicker3("Color", colors[playerIndex],ImGuiColorEditFlags_NoPicker || ImGuiColorEditFlags_NoOptions || ImGuiColorEditFlags_NoSmallPreview ||  ImGuiColorEditFlags_NoInputs ||  ImGuiColorEditFlags_NoTooltip ||  ImGuiColorEditFlags_NoLabel || ImGuiColorEditFlags_NoSidePreview || ImGuiColorEditFlags_NoDragDrop))
+		{
+			int r = colors[playerIndex][0] * 255;
+			int g = colors[playerIndex][1] * 255;
+			int b = colors[playerIndex][2] * 255;
+			colors_hex[playerIndex] = ((0xff) << 24) + ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+		}
+		ImGui::TreePop();
+	}
 	if (Input::Get()->WasKeyPressed(VK_F1))
 	{
 		enabled = !enabled;
